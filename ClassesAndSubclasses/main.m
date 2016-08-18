1dc483daa0391d72c5254574e8fbe24c6a28772f
Princess Sampson <ps@kingonomics.com>
2016-08-17 23:55:38 -0400

    fixed spelling of Di's name

--- ClassesAndSubclasses/main.m
+++ ClassesAndSubclasses/main.m
@@ -14,7 +14,7 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        ForceUser *forceUser = [[ForceUser alloc] initWithMidichlorianCount:5000 name:@"Imma Gun Di"];
+        ForceUser *forceUser = [[ForceUser alloc] initWithMidichlorianCount:5000 name:@"Ima-Gun Di"];
         Jedi *anakin = [[Jedi alloc] initWithMidichlorianCount:20000 name:@"Anakin Skywalker" lightsaberColor:@"blue"];
         Padawan *ahsoka = [[Padawan alloc] initWithMidichlorianCount:10000 name:@"Ahsoka Tano" lightsaberColor:@"green" mastersName:@"Anakin Skywalker"];
         
